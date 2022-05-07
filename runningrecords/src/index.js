271a75f2b8a4753f74d44153595b8386d5dd6ac6
ndescussebrown <nathalie.descusse@gmail.com>
2022-05-07 22:09:25 +0200

    fix: fixed issue with redis connection due to breaking change of node redis version

--- runningrecords/src/index.js
+++ runningrecords/src/index.js
@@ -6,6 +6,19 @@ const methodOverride = require('method-override');
 const redis = require('redis');
 
 
+// Create Redis Client
+const client = redis.createClient();
+
+client.on("error", function (err) {
+    console.error("Errro encountered: ", err);
+});
+
+client.on("connect", () => {
+    console.log("Redis connected");
+});
+
+client.connect(); // connect to the server here!
+
 // Set port
 const port = 3000;
 
@@ -24,12 +37,31 @@ app.use(bodyParser.urlencoded({extended:false}));
 // medthodOverrride
 app.use(methodOverride('_method'));
 
+//Search page
 app.get('/', function(req, res, next){
     res.render('searchrecords');
 });
 
+//Search processing
+app.post('/record/search', function (req, res, next){
+    let id = req.body.id;
+
+    client.hgetall(id, function (err, obj) {
+        if (!obj) {
+            res.render('searchrecords', {
+                error: 'Record does not exist'
+            });
+        } else {
+            obj.id = id;
+            res.render('details', {
+                record:obj
+            });
+        }
+    });
+});
+
 app.listen(port, function(){
-    console.log("Server listening the port " +port)
+    console.log("Server listening to port " +port)
 });
 
 
